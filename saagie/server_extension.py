dd69870862853b525c5f4db903779220ca2c10e9
Bertrand Bordage <bordage.bertrand@gmail.com>
2017-02-11 12:28:09 +0100

    Makes sure dependencies are installed before the plugin + fixes Python 2 install.

---
+++ saagie/server_extension.py
@@ -0,0 +1,81 @@
+import json
+import re
+
+from jinja2 import Environment, PackageLoader, TemplateNotFound
+from notebook.utils import url_path_join
+from notebook.base.handlers import IPythonHandler
+import requests
+
+
+env = Environment(
+    loader=PackageLoader('saagie', 'jinja2'),
+)
+session = requests.Session()
+
+
+class SaagieHandler(IPythonHandler):
+    def get(self):
+        data = self.request.arguments.copy()
+        try:
+            template = data.pop('template')[0].decode()
+        except (KeyError, IndexError):
+            template = ''
+        try:
+            template = env.get_template(template)
+        except TemplateNotFound:
+            self.send_error(404)
+        self.finish(template.render({k: v[0].decode()
+                                     for k, v in data.items()}))
+
+
+class SaagieCheckHandler(IPythonHandler):
+    def get(self):
+        self.finish()
+
+
+CONTAINER_RE = re.compile(r'^https?://[^/]+\.prod\.saagie\.io(?:\Z|/.*)$')
+
+
+class SaagieProxyHandler(IPythonHandler):
+    def post(self):
+        data = self.request.arguments.copy()
+        try:
+            url = data.pop('url')[0].decode()
+        except (KeyError, IndexError):
+            url = ''
+        if url.startswith('/'):
+            url = 'https://manager.prod.saagie.io' + url
+        elif CONTAINER_RE.match(url) is None:
+            self.send_error(404)
+            return
+        try:
+            method = data.pop('method')[0].decode()
+        except (KeyError, IndexError):
+            method = 'GET'
+        try:
+            json_data = json.loads(data.pop('json')[0].decode())
+        except (KeyError, IndexError, ValueError):
+            json_data = None
+        response = session.request(method, url, data=data, json=json_data,
+                                   timeout=5)
+        if response.status_code != 200:
+            self.send_error(response.status_code)
+            return
+        self.finish(response.text)
+
+    def check_xsrf_cookie(self):
+        return
+
+
+def load_jupyter_server_extension(nb_app):
+    web_app = nb_app.web_app
+    base_url = web_app.settings['base_url']
+
+    route_pattern = url_path_join(base_url, '/saagie')
+    web_app.add_handlers('.*$', [(route_pattern, SaagieHandler)])
+
+    route_pattern = url_path_join(base_url, '/saagie/check')
+    web_app.add_handlers('.*$', [(route_pattern, SaagieCheckHandler)])
+
+    route_pattern = url_path_join(base_url, '/saagie/proxy')
+    web_app.add_handlers('.*$', [(route_pattern, SaagieProxyHandler)])
--- setup.py
+++ setup.py
@@ -4,51 +4,64 @@ import os
 from setuptools import setup
 from setuptools.command.install import install
 
-from notebook.nbextensions import install_nbextension
-from notebook.services.config import ConfigManager
-from jupyter_core.paths import jupyter_config_dir
+from saagie import __version__
+
+CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
+EXTENSION_PATH = os.path.join(CURRENT_PATH, 'saagie')
+
+with open(os.path.join(CURRENT_PATH, 'requirements.txt')) as f:
+    required = f.read().splitlines()
 
 
-EXTENSION_PATH = os.path.join(os.path.dirname(__file__), 'saagie')
+def setup_extensions():
+    try:
+        from notebook.nbextensions import install_nbextension
+        from notebook.services.config import ConfigManager
+        from jupyter_core.paths import jupyter_config_dir
+    except ImportError:
+        raise ImportError('Install Jupyter notebook before.')
+
+    # Installs JS extensions to ~/.local/share/jupyter/nbextensions
+    install_nbextension(EXTENSION_PATH, overwrite=True, user=True)
+
+    # Enables the JS extension.
+    js_cm = ConfigManager()
+    section = 'notebook'
+    cfg = js_cm.get(section)
+    extensions = cfg['load_extensions'] = cfg.setdefault('load_extensions',
+                                                         {})
+    extensions['saagie/saagie'] = True
+    js_cm.update(section, cfg)
+
+    # Enables the Python server extension.
+    server_cm = ConfigManager(write_config_dir=jupyter_config_dir())
+    section = 'jupyter_notebook_config'
+    cfg = server_cm.get(section)
+    app = cfg['NotebookApp'] = cfg.setdefault('NotebookApp', {})
+    server_extensions = app['server_extensions'] = \
+        app.setdefault('server_extensions', [])
+    server_extension = 'saagie.server_extension'
+    if server_extension not in server_extensions:
+        server_extensions.append(server_extension)
+    server_cm.update(section, cfg)
 
 
 class InstallCommand(install):
     def run(self):
         # Installs Python package & its dependencies.
-        super(InstallCommand, self).run()
+        install.do_egg_install(self)
 
-        # Installs JS extensions to ~/.local/share/jupyter/nbextensions
-        install_nbextension(EXTENSION_PATH, overwrite=True, user=True)
-
-        # Enables the JS extension.
-        js_cm = ConfigManager()
-        section = 'notebook'
-        cfg = js_cm.get(section)
-        extensions = cfg['load_extensions'] = cfg.setdefault('load_extensions',
-                                                             {})
-        extensions['saagie/saagie'] = True
-        js_cm.update(section, cfg)
-
-        # Enables the Python server extension.
-        server_cm = ConfigManager(write_config_dir=jupyter_config_dir())
-        section = 'jupyter_notebook_config'
-        cfg = server_cm.get(section)
-        app = cfg['NotebookApp'] = cfg.setdefault('NotebookApp', {})
-        server_extensions = app['server_extensions'] = \
-            app.setdefault('server_extensions', [])
-        server_extension = 'saagie'
-        if server_extension not in server_extensions:
-            server_extensions.append(server_extension)
-        server_cm.update(section, cfg)
+        self.execute(setup_extensions, (),
+                     msg='Installing Jupyter JS & server extensions...')
 
 
 setup(
     name='jupyter-saagie-plugin',
-    version='0.1',
+    version=__version__,
     packages=['saagie'],
     cmdclass={
         'install': InstallCommand,
     },
-    install_requires=['requests==2.12.5'],
+    install_requires=required,
     include_package_data=True,
 )
